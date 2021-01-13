0a21f38e83336e8e35831b3eebd18472a11dec2a
Alessandro Mamusa <alesardo@develer.com>
2021-01-13 23:34:20 +0100

    add car acceleration and friction

--- Car.gd
+++ Car.gd
@@ -4,13 +4,22 @@ extends KinematicBody2D
 
 var wheel_base = 70 # distance between the wheels
 var steering_angle = 15
+var engine_power = 800
 
+var acceleration = Vector2.ZERO
 var velocity = Vector2.ZERO
 var steer_direction
 
+var friction = -0.9
+var drag = -0.0015
+
+
 func _physics_process(delta):
+	acceleration = Vector2.ZERO
 	get_input()
+	apply_friction()
 	calculate_steering(delta)
+	velocity += acceleration * delta
 	velocity = move_and_slide(velocity)
 
 
@@ -22,9 +31,9 @@ func get_input():
 		turn -= 1
 
 	steer_direction = turn * deg2rad(steering_angle)
-	velocity = Vector2.ZERO
+
 	if Input.is_key_pressed(KEY_UP):
-		velocity = transform.x * 500
+		acceleration = transform.x * engine_power
 
 
 func calculate_steering(delta):
@@ -37,3 +46,13 @@ func calculate_steering(delta):
 	var new_heading = (front_wheel - rear_wheel).normalized()
 	velocity = new_heading * velocity.length()
 	rotation = new_heading.angle()
+
+
+func apply_friction():
+	if velocity.length() < 5:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction
+	var drag_force = velocity * velocity.length() * drag
+	if velocity.length() < 100:
+		friction_force *= 3
+	acceleration += drag_force + friction_force
